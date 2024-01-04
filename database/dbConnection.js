031fb9f213f8e8bccfdd84279b20764c05566b9b
Роман Колесников <romankolesnikov@Romans-MacBook-Pro.local>
2024-01-05 01:56:59 +0200

    add env file

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,11 +1,16 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  user: "admin",
-  password: "root",
-  host: "localhost",
-  port: "5432:5432",
-  database: "postgres",
+  // user: "admin",
+  // password: "root",
+  // host: "localhost",
+  // port: "5432:5432",
+  // database: "postgres",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DATA_BASE,
 });
 
 module.exports = pool;
